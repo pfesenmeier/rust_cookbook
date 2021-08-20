f46d69ddbaae5946cc124480a278160c93fe1c27
Paul Fesenmeier <fesenmeier_paul@network.lilly.com>
2021-08-20 16:57:53 -0400

    Add example from Std::fs::File

---
+++ read_file.rs
@@ -0,0 +1,14 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("foo.txt")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+
+    buf_reader.read_to_string(&mut contents)?;
+
+    assert_eq!(contents, "Hello, world!");
+    Ok(())
+}
