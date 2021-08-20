c8743a25750a1cf62d6aabba299061e506b1877c
Paul Fesenmeier <fesenmeier_paul@network.lilly.com>
2021-08-20 17:31:56 -0400

    Add command-line logic

--- read_file.rs
+++ read_file.rs
@@ -1,14 +1,18 @@
+use std::env;
 use std::fs::File;
-use std::io::BufReader;
 use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() -> std::io::Result<()> {
-    let file = File::open("foo.txt")?;
-    let mut buf_reader = BufReader::new(file);
-    let mut contents = String::new();
+    let mut args: Vec<String> = env::args().skip(1).collect();
+    let file_name = args.pop();
+    if let Some(name) = file_name {
+        let file = File::open(name)?;
+        let mut buf_reader = BufReader::new(file);
+        let mut contents = String::new();
 
-    buf_reader.read_to_string(&mut contents)?;
-
-    assert_eq!(contents, "Hello, world!");
+        buf_reader.read_to_string(&mut contents)?;
+        println!("{}", contents);
+    }
     Ok(())
 }
